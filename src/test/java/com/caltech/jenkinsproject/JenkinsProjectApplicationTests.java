d2c69a7ebca5727cf80e91783311bfbb40cb58e6
yhanna19 <tootyah101@g.ucla.edu>
2021-02-07 22:31:25 +0000

    Added unit tests

--- src/test/java/com/caltech/jenkinsproject/JenkinsProjectApplicationTests.java
+++ src/test/java/com/caltech/jenkinsproject/JenkinsProjectApplicationTests.java
@@ -2,12 +2,51 @@ package com.caltech.jenkinsproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import orq.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 @SpringBootTest
 class JenkinsProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static String Base_URL = "https://www.facebook.com";
+	private WebDriver driver;
+
+	@Before 
+	public void setUp()
+	{
+		driver = new ChromeDriver();
+		driver.get(Base_URL);
 	}
 
+	@After
+	public void after()
+	{
+		driver.quit();
+	}
+
+	@Test
+	public void testCasePassed() 
+	{
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@id='login_form']")).isDisplayed());
+	}
+
+	@Test
+	public void testCaseFailed()
+	{
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@id='failed case']")).isDisplayed());
+	}
+
+	@Ignore
+	@Test
+	public void testCaseIgnored()
+	{
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@id='ignored case']")).isDisplayed());
+	}
 }
