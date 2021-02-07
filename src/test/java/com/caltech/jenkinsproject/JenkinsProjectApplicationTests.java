fe3a06690343f255276b5cf14630b7a1d28e35c2
yhanna19 <tootyah101@g.ucla.edu>
2021-02-07 21:19:43 +0000

    First commit, added springboot project

---
+++ src/test/java/com/caltech/jenkinsproject/JenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.jenkinsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
