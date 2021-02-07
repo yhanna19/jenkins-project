d1e7f7acc6e1edf542774ab0e9de9b486939c240
yhanna19 <tootyah101@g.ucla.edu>
2021-02-07 22:49:27 +0000

    Fixed Webdriver typo

--- src/test/java/com/caltech/jenkinsproject/JenkinsProjectApplicationTests.java
+++ src/test/java/com/caltech/jenkinsproject/JenkinsProjectApplicationTests.java
@@ -6,9 +6,8 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.Test;
 import org.openqa.selenium.By;
-import orq.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
