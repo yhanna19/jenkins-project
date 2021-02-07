bad909926f3eca7406576baade2aae619449eaf9
yhanna19 <tootyah101@g.ucla.edu>
2021-02-07 23:33:29 +0000

    Removed uneccessary steps to unit test

--- src/test/java/com/caltech/jenkinsproject/JenkinsProjectApplicationTests.java
+++ src/test/java/com/caltech/jenkinsproject/JenkinsProjectApplicationTests.java
@@ -2,50 +2,12 @@ package com.caltech.jenkinsproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 
 @SpringBootTest
 class JenkinsProjectApplicationTests {
 
-	 private static String Base_Url = "https://www.facebook.com";
-    private WebDriver driver;
+	@Test
+	void contextLoads() {
+	}
 
-    @Before
-    public void setUp()
-    {
-        driver = new ChromeDriver();
-        driver.get(Base_Url);
-    }
-
-    @After
-    public void after()
-    {
-        driver.quit();
-    }
-
-    @Test
-    public void testCasePassed()
-    {
-        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='login_form']")).isDisplayed());
-    }
-
-    @Test
-    public void testCaseFailed()
-    {
-        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='failed case']")).isDisplayed());
-    }
-
-    @Ignore
-    @Test
-    public void testCaseIgnored()
-    {
-        Assert.assertTrue(driver.findElement(By.xpath("//form[@id='ignored case']")).isDisplayed());
-    }
 }
